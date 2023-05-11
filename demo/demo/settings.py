4e6c05b5b82b1c562002cf311d5e45f98d79b135
sageerkv <sageerkv93@gmail.com>
2023-05-11 13:24:58 +0530

    Update settings.py

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -149,9 +149,9 @@ AUTHENTICATION_BACKENDS = ['demo_app.backends.EmailBackend']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FORM = 'citycarehospital996@gmail.com'
-EMAIL_HOST_USER = 'citycarehospital996@gmail.com'
-EMAIL_HOST_PASSWORD = 'tgualcjrlvxblbgr'
+EMAIL_FORM = 'demo@gmail.com'
+EMAIL_HOST_USER = 'demo@gmail.com'
+EMAIL_HOST_PASSWORD = '**************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
